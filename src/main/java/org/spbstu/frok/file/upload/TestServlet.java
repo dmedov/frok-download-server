691d96dfe8b184553547120826f2f38634be6426
Denis Sermukhamedov <den.medov@gmail.com>
2014-03-17 01:27:34 +0400

    add TestServlet (upload file and simple get)

---
+++ src/main/java/org/spbstu/frok/file/upload/TestServlet.java
@@ -0,0 +1,57 @@
+package org.spbstu.frok.file.upload;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/api"})
+@MultipartConfig(location = "/tmp")
+public class TestServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        for (Part part : request.getParts()) {
+            part.write(part.getSubmittedFileName());
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Test get</title>");
+            out.println("</head>");
+            out.println("<body>");
+            String user = request.getParameter("user");
+
+            if (user != null) {
+                out.println("hello "+ user);
+            }
+
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "TestServlet";
+    }
+}
