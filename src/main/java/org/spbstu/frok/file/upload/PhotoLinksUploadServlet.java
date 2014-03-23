55a4a931dc86b5a70ce4571c554ebdeb10de9429
Denis Sermukhamedov <den.medov@gmail.com>
2014-03-24 00:27:18 +0400

    add download image by link function

--- src/main/java/org/spbstu/frok/file/upload/PhotoLinksUploadServlet.java
+++ src/main/java/org/spbstu/frok/file/upload/PhotoLinksUploadServlet.java
@@ -1,5 +1,11 @@
 package org.spbstu.frok.file.upload;
 
+import org.apache.commons.io.FileUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -7,21 +13,51 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.URL;
+import java.util.Iterator;
 
 @WebServlet(urlPatterns = {"/links"})
 @MultipartConfig(location = "/tmp")
 public class PhotoLinksUploadServlet extends HttpServlet {
+    private static final String UPLOAD_DIRECTORY = "/tmp/";
+
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        String filename = null;
 
         for (Part part : request.getParts()) {
             String submittedFileName = part.getSubmittedFileName();
             if (submittedFileName != null) {
                 part.write(submittedFileName);
+                filename = submittedFileName;
             }
+        }
+
+        JSONParser parser = new JSONParser();
+
+        try {
+            Object obj = parser.parse(new FileReader("/tmp/" + filename));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            String userId = (String) jsonObject.get("user_id");
+
+            // parse photo links
+            JSONArray photos = (JSONArray) jsonObject.get("photos");
+            Iterator<String> iterator = photos.iterator();
+            while (iterator.hasNext()) {
+                FileUtils.copyURLToFile(new URL(iterator.next()), new File(UPLOAD_DIRECTORY + userId + File.separator + "test.png"));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
     }
 
